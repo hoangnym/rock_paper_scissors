1761640f3317dc56e7f5cdb51e142b92109be2ff
Minh Hoang <hoangnym94@gmail.com>
2019-12-12 15:04:19 +0100

    rock paper scissors game first design inspired be dev ed

---
+++ app.js
@@ -0,0 +1,106 @@
+// Create a few functions
+const game = ()=> {
+    let pScore = 0;
+    let cScore = 0;
+
+    //Start the game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match')
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
+
+    //Play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        //Computer Options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option=>{
+            option.addEventListener('click', function() {
+                //Computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                //Here is where we call compare hands
+                compareHands(this.textContent, computerChoice);
+                //Update Images
+                playerHand.src =  `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
+            })
+        })
+        
+    };
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p')
+        const computerScore = document.querySelector('.computer-score p')
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
+
+    const compareHands = (playerChoice, computerChoice) => {
+        //Update Text
+        const winner = document.querySelector('.winner')
+        //Checking for a tie
+        if(playerChoice === computerChoice) {
+            winner.textContent = 'It is a tie.';
+            return;
+        }
+        //Check for rock
+        if(playerChoice === 'rock'){
+            if(computerChoice === 'scissors') {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        //Check for paper
+        if(playerChoice === 'paper'){
+            if(computerChoice === 'rock') {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        //Check for scissors
+        if(playerChoice === 'scissors'){
+            if(computerChoice === 'paper') {
+                winner.textContent = 'Player Wins';
+                pScore++;
+                updateScore();
+                return;
+            }else{
+                winner.textContent = 'Computer Wins';
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+    };
+
+    // Is call all the inner function
+    startGame();
+    playMatch();
+};
+
+// start the game function
+game();
